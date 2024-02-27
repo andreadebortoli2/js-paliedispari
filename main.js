42b6eb686aeb4f2605bdc42caa6ee08eed0c7d6f
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-27 15:45:50 +0100

    add word request and set as an array

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('HI');
+// console.log('HI');
+
+// -ask user for a word
+
+// const userWord = prompt('write a word');
+const userword = 'ciao';
+// const userWord = 'anna'; palindroma
+
+// -set the word as an array:
+//     -split the word in letters
+//     -push each letter as an element of the array
+
+const lettersList = []
+
+lettersList.push(userword.split(''))
+
+console.log(lettersList);
+
+// -check if the word is a palindrome
+//     -read the array in reverse
+//     -find if the array reverse is equal to the normalò array
+// -print the result
+
