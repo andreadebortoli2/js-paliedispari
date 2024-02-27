6a62c42f4cc6cd57d1e154669f1194553a34e7a4
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-02-27 16:24:50 +0100

    edit the exercise 1  as a function

--- main.js
+++ main.js
@@ -3,21 +3,18 @@
 // -ask user for a word
 
 // const userWord = prompt('write a word');
-const userWord = 'ciao';
-// const userWord = 'anna'; // palindroma
 
 // -set the word as an array:
 //     -split the word in letters
 //     -push each letter as an element of the array
 
-const lettersList = []
+/* const lettersList = [];
 
 for (let i = 0; i < userWord.length; i++) {
     const element = userWord[i];
-    
-    lettersList.push(element)
 
-}
+    lettersList.push(element);
+}; */
 
 // console.log(lettersList);
 
@@ -28,22 +25,51 @@ for (let i = 0; i < userWord.length; i++) {
 
 //     -find if the array reverse is equal to the normal array
 
-let palindromeCheck = false
+/* let palindromeCheck = false;
 
 for (let i = 0; i < lettersList.length; i++) {
-    
+
     if (lettersList[i] === lettersList.reverse()[i]) {
-        palindromeCheck = true
-    }
-}
+        palindromeCheck = true;
+    };
+}; */
 
 // -print the result
 
-if (palindromeCheck === true) {
+/* if (palindromeCheck === true) {
     console.log('ok');
 } else {
     console.log('no');
-}
+}; */
 
 
 // -set the exercise as a function
+
+
+function palindromeCheck() {
+    
+    const userWord = prompt('write a word');
+
+    const lettersList = [];
+    
+    for (let i = 0; i < userWord.length; i++) {
+        const element = userWord[i];
+    
+        lettersList.push(element);
+    };
+
+    for (let i = 0; i < lettersList.length; i++) {
+
+        if (lettersList[i] === lettersList.reverse()[i]) {
+            return true;
+        };
+    };
+}
+
+let resultMessage = 'The word is NOT a palindrome';
+
+if (palindromeCheck() === true) {
+    resultMessage = 'The word IS a plaindrome';
+};
+
+alert(resultMessage);
